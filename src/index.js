b2d394b985a9170c6bb332a98dba83c7014c58cd
Christopher Meyer <chris@schwiiz.org>
2020-08-05 21:03:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+module.exports = promiseTimeout(promise, timeout, errMsg = 'The operation timed out.') => {
+
+	let timeoutID
+
+	const timeOutPromise = new Promise((resolve, reject) => {
+		timeoutID = setTimeout(() => reject(errMsg), timeout)
+	})
+
+	return Promise.race([promise, timeOutPromise])
+		.finally(() => clearTimeout(timeoutID))
+
+}
