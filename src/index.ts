e94364938a60d3020a7369b17c57a4311d1059df
Christopher Meyer <chris@schwiiz.org>
2024-10-04 17:42:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 const promiseRetry = <T>(
   promise: Promise<T>,
   timeout: number,
-  errMsg = "The promise timed out."
+  errMsg = "The promise timed out.",
 ): Promise<T> => {
   let timeoutID;
 
-  // The <T> here to keep TS happy.
   const timeOutPromise = new Promise<T>(
     (_resolve, reject) =>
-      (timeoutID = setTimeout(() => reject(new Error(errMsg)), timeout))
+      (timeoutID = setTimeout(() => reject(new Error(errMsg)), timeout)),
   );
 
   return Promise.race([promise, timeOutPromise]).finally(() =>
-    clearTimeout(timeoutID)
+    clearTimeout(timeoutID),
   );
 };
 
